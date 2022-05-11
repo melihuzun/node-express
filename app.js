dbf82f95ca8bd1e6f049bad6bbef93b92788f0e1
Melih Uzun <melihuzun@protonmail.com>
2022-05-11 03:59:03 +0300

    set-up port

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-const express = require('express')
+const express = require("express");
 
 //controllers
 
 // Routers
-const usersRouter = require('./routes/users.router')
+const usersRouter = require("./routes/users.router");
 
-const app = express()
-
+const app = express();
 
 // MiddleWares
-app.use(express.static('public'))
-app.use(express.json())
-app.use('/users',usersRouter);
+app.use(express.static("public"));
+app.use(express.json());
+app.use("/users", usersRouter);
 
-app.get("/", (req,res) => {
-	res.sendFile(__dirname+"/views/index.html")
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-app.listen(3000,() =>{
-	console.log("running on http://localhost/3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("running on http://localhost/3000");
 });
